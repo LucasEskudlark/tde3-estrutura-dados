cb6cc484d27f389d3111fc45273291afdd06c778
Lucas Eskudlark <lucaseskudlark@gmail.com>
2022-05-09 08:56:30 -0300

    Criado o projeto e adicionado os arquivos ArvoreBin e NoAbin

---
+++ src/NoAbin.java
@@ -0,0 +1,11 @@
+public class NoAbin {
+    public NoAbin esquerda;
+    public NoAbin direita;
+    public int info;
+
+    public NoAbin(int valor) {
+        this.info = valor;
+        this.esquerda = null;
+        this.direita = null;
+    }
+}
